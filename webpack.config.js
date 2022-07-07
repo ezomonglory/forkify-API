ac4bbf96882252fe68a426f664c777cdf591e083
Ezomon Glory <ezomonglory01@gmail.com>
2022-07-07 22:52:15 +0100

    renamed dist folder to public

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ const path = require('path');
 module.exports = {
     entry: "./src/index.js",
     output :{
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'js/bundle.js'
     },
     
     devServer: {
-        static :"./dist"
+        static :"./public"
         
     }
 }
